df63397d09b58ae4eca77aec715e8c549bfcaf15
kscomac <90888432+kscomac@users.noreply.github.com>
2021-11-30 15:01:39 +0900

    Update index.php

--- index.php
+++ index.php
@@ -16,14 +16,14 @@ if (isset($_GET['result']) && is_numeric($_GET['result']) && $_GET['result'] >= 
     <meta property="og:site_name" content="マルサンアイ株式会社｜豆乳と味噌メーカー">
     <meta property="og:title" content="いつでもどこでも「鮮度みそ占い」キャンペーン｜マルサンアイ株式会社">
     <meta property="og:description" content="いつでもどこでも「鮮度みそ占い」キャンペーン 占い結果をツイートで総勢100名様に「巨大！鮮度みそクッション」などが当たる！！">
-    <meta property="og:image" content="https://github.com/kscomac/maru/<?php echo $pic ?>.jpg">
+    <meta property="og:image" content="https://kscomac.github.io/maru/<?php echo $pic ?>.jpg">
     <meta property="og:url" content="http://localhost/cp_miso_202112/index.php?result=<?php echo $result ?>">
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:site" content="マルサンアイ株式会社｜豆乳と味噌メーカー" />
     <meta name="twitter:title" content="いつでもどこでも「鮮度みそ占い」キャンペーン｜マルサンアイ株式会社" />
     <meta name="twitter:description" content="いつでもどこでも「鮮度みそ占い」キャンペーン 占い結果をツイートで総勢100名様に「巨大！鮮度みそクッション」などが当たる！！" />
-    <meta name="twitter:image" content="https://github.com/kscomac/maru/<?php echo $pic ?>.jpg">
-    <meta name="twitter:url" content="http://localhost/cp_miso_202112/index.php?result=<?php echo $result ?>">
+    <meta name="twitter:image" content="https://kscomac.github.io/maru/<?php echo $pic ?>.jpg">
+    <meta name="twitter:url" content="https://kscomac.github.io/maru/index.php?result=<?php echo $result ?>">
 <?php }else{ ?>
     <meta property="og:type" content="article">
     <meta property="og:site_name" content="サイトネーム">
